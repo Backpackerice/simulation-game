e432d468f1ac2360bc0aa6a360e6265379b5c1ec
Jan Bussieck <jan.bussieck@gmail.com>
2015-01-21 18:31:54 +0100

    massive clean up of front ent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,41 +4,41 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   #around filter
-  around_action :wrap_application
-
-  def wrap_application
-    update_game_data
-    simulate_risk
-    yield
-    process_events
-  end
-
-  # returns an instance of the singleton queue
-  def get_queue
-    Queue.instance
-  end
-  helper_method :get_queue
-
-  private
-    def update_game_data
-      # increase number of turns and Date
-    end
-
-    def simulate_risks
-      #
-    end
-
-    def process_events
-      threads = []
-      5.times do
-        threads << Thread.new do
-          while event = get_queue.pop
-            Worker.new.process(event)
-          end
-        end
-      end
-      threads.each {|thr| thr.join}
-      #while events in qeue
-    end
+  # around_action :wrap_application
+  #
+  # def wrap_application
+  #   update_game_data
+  #   simulate_risk
+  #   yield
+  #   process_events
+  # end
+  #
+  # # returns an instance of the singleton queue
+  # def get_queue
+  #   Queue.instance
+  # end
+  # helper_method :get_queue
+  #
+  # private
+  #   def update_game_data
+  #     # increase number of turns and Date
+  #   end
+  #
+  #   def simulate_risks
+  #     # run monte carlo simulation
+  #   end
+  #
+  #   def process_events
+  #     threads = []
+  #     5.times do
+  #       threads << Thread.new do
+  #         while event = get_queue.pop
+  #           Worker.new.process(event)
+  #         end
+  #       end
+  #     end
+  #     threads.each {|thr| thr.join}
+  #     #while events in qeue
+  #   end
 
 end
